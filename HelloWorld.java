fd5dc2edbd2dca7cb4f46bb8f93e73f66e59f5a1
Chandra <chandra.kleinhans@gmail.com>
2023-05-03 19:33:04 -0500

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("MyName is Coding Dojo");
+        System.out.println("I am 100 years old");
+        System.out.println("My hometown is Burbank, CA");
+    }
+}
